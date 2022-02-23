d845e2d269387141e7bee0ae35cd111da1980655
xunyi24 <liupf24@163.com>
2022-02-23 14:51:32 +0800

    update

---
+++ src/main/java/com/moonflying/timekiller/core/DispatcherServer.java
@@ -0,0 +1,62 @@
+package com.moonflying.timekiller.core;
+
+import com.moonflying.timekiller.core.handler.DispatcherHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class DispatcherServer implements ApplicationRunner {
+    private int port;
+
+    public DispatcherServer(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //创建两个线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(); //8个NioEventLoop
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+
+                            //获取到pipeline
+                            ChannelPipeline pipeline = ch.pipeline();
+                            //向pipeline加入解码器
+                            pipeline.addLast("decoder", new StringDecoder());
+                            //向pipeline加入编码器
+                            pipeline.addLast("encoder", new StringEncoder());
+                            //加入自己的业务处理handler
+                            pipeline.addLast(new DispatcherHandler());
+
+                        }
+                    });
+
+            System.out.println("netty 服务器启动");
+            ChannelFuture channelFuture = b.bind(port).sync();
+
+            //监听关闭
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
---
+++ src/main/java/com/moonflying/timekiller/core/handler/DispatcherHandler.java
@@ -0,0 +1,11 @@
+package com.moonflying.timekiller.core.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class DispatcherHandler extends SimpleChannelInboundHandler {
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+
+    }
+}
