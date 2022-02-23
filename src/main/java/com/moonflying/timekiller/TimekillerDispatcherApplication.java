e241daccfdb8c8eda51f5b1871e1e1bfa75b7cd6
xunyi24 <liupf24@163.com>
2022-02-23 08:40:28 +0800

    init proj

---
+++ src/main/java/com/moonflying/timekiller/TimekillerDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.moonflying.timekiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekillerDispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimekillerDispatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moonflying/timekiller/TimekillerDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonflying.timekiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimekillerDispatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
